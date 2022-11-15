dc5d32e8db69b95fa567cd66a335c25d66dced2d
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-15 13:08:04 -0800

    edit app.js layout

--- app.js
+++ app.js
@@ -3,35 +3,64 @@
 // console.log(cardValue)
 // console.log(hitButton)
 // console.log(stayButton)
+//console.log(Math.floor(Math.random() * (cardValue[i] + cardSuit[k])))
 
+//CARD DECK AND SUITS STORED IN VARIABLE
+let cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q']
+let cardSuit = ['H', 'S', 'D', 'C']
+let cardDeck = []
 
 // BUTTONS STORED IN A VARIABLE 
 let hitButton = document.querySelector('#hitButton')
 let stayButton = document.querySelector('#stayButton')
 
+//HIT BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
 hitButton.addEventListener('click', () => {
     for(let i=0; i < cardValue.length; i++){
         for (let k = 0; k < cardSuit.length; k++){
-        console.log(cardValue[i] + cardSuit[k])
+       console.log(Math.floor(Math.random() * (cardValue[i] + cardSuit[k])))
     }
 }
-    
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//STAY BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
 stayButton.addEventListener('click', () => {
 
 })
 
-//CARD DECK AND SUITS STORED IN VARIABLE
-let cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q']
-let cardSuit = ['H', 'S', 'D', 'C']
-   
+
+//LOOPING THROUGH THE SUITS AND VALUES TO FORM A DECK 
 // for(let i=0; i < cardValue.length; i++){
 //         for (let k = 0; k < cardSuit.length; k++){
-//         console.log(cardValue[i] + cardSuit[k])
+//         cardDeck.push(values[i] + values[k])
 //     }
 // }
-
 
 
 
