1b5f25fcf1a4906797bc6c69a7e4533c5a3423d5
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-15 11:25:47 -0800

    add card deck loop

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+// console.log("hello world")
+// console.log(cardSuit)
+// console.log(cardValue)
+
+//CARD DECK AND SUITS STORED IN VARIABLE
+let cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q']
+let cardSuit = ['H', 'S', 'D', 'C']
+
+// FUNCTION THAT LOOPS THROUGH ARRAYS AND GIVES OUTPUT FOR HIT ME BUTTON 
+for(let i=0; i < cardValue.length; i++){
+    for (let k = 0; k < cardSuit.length; k++){
+        console.log(cardValue[i] + cardSuit[k])
+    }
+}
+
+
