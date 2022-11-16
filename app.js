c8190f5f6e3bbbd6f0d674a5ac3ec870e277e45f
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-16 08:52:41 -0800

    Shuffle deck

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 //console.log(Math.floor(Math.random() * (cardValue[i] + cardSuit[k])))
 //console.log(cardDeck);
 // newDeck();
+//shuffleDeck();
 
 //CARD DECK AND SUITS STORED IN VARIABLE
 let cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q'];
@@ -25,24 +26,34 @@ let stayButton = document.querySelector('#stayButton')
 function newDeck(){
     for(let i=0; i < cardValue.length; i++){
         for (let k = 0; k < cardSuit.length; k++){
-           cardDeck.push(cardValue[i] + cardSuit[k])
+          cardDeck.push(cardValue[i] + cardSuit[k])
         }
     }
-    console.log(cardDeck);
 }
+newDeck();
 
-// newDeck();
-
-
-//HIT BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
-hitButton.addEventListener('click', () => {
-    for(let i=0; i < cardValue.length; i++){
-        for (let k = 0; k < cardSuit.length; k++){
-       console.log(Math.floor(Math.random() * (cardValue[i] + cardSuit[k])))
+// A FUNCTION THAT SHUFFLES MY DECK 
+function shuffleDeck (){
+    let currentIndex = cardDeck.length, randomIndex;
+    while (currentIndex !==0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [cardDeck[currentIndex], cardDeck[randomIndex]] = [cardDeck[randomIndex], cardDeck[currentIndex]];
     }
+    console.log(cardDeck)
+   return cardDeck
 }
-})
+shuffleDeck();
 
+//HIT BUTTON THAT GENERATES RANDOM 2 CARDS TO PLAYER 
+hitButton.addEventListener('click', () => {
+    //we dont want to call the shuffle deck because it will give more cards than necessary we could use a function that only provides two cards at a time of game start then 1 card there after 
+
+    // game start two cards auto populate 
+    // then it is up to player to hit or stay
+    // during case of hit the player should get a card until the total value is 21 < 
+})
+   
 
 //STAY BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
 stayButton.addEventListener('click', () => {
