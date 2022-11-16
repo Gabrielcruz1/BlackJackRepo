c692d4916985b49fd2bd1ea97d96b6677fbad94a
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-15 21:32:59 -0800

    Card Deck Make

--- app.js
+++ app.js
@@ -4,15 +4,34 @@
 // console.log(hitButton)
 // console.log(stayButton)
 //console.log(Math.floor(Math.random() * (cardValue[i] + cardSuit[k])))
+//console.log(cardDeck);
 
 //CARD DECK AND SUITS STORED IN VARIABLE
-let cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q']
-let cardSuit = ['H', 'S', 'D', 'C']
-let cardDeck = []
+let cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q'];
+let cardSuit = ['H', 'S', 'D', 'C'];
+let cardDeck = [];
+
+// SCORE COUNTER BETWEEN DEALER AND PLAYER 
+let dealerTotal = 0;
+let playerTotal = 0;
 
 // BUTTONS STORED IN A VARIABLE 
 let hitButton = document.querySelector('#hitButton')
 let stayButton = document.querySelector('#stayButton')
+
+
+//LOOPING THROUGH THE SUITS AND VALUES TO FORM A DECK 
+function newDeck(){
+    for(let i=0; i < cardValue.length; i++){
+        for (let k = 0; k < cardSuit.length; k++){
+           cardDeck.push(cardValue[i] + cardSuit[k])
+        }
+    }
+    console.log(cardDeck);
+}
+
+newDeck();
+
 
 //HIT BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
 hitButton.addEventListener('click', () => {
@@ -24,43 +43,13 @@ hitButton.addEventListener('click', () => {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //STAY BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
 stayButton.addEventListener('click', () => {
 
 })
 
 
-//LOOPING THROUGH THE SUITS AND VALUES TO FORM A DECK 
-// for(let i=0; i < cardValue.length; i++){
-//         for (let k = 0; k < cardSuit.length; k++){
-//         cardDeck.push(values[i] + values[k])
-//     }
-// }
+
 
 
 
