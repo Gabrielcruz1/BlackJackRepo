af6cc6d434192e965f162d40148bb0dd67231383
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-17 10:25:53 -0800

    Window onload function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //CARD DECK AND SUITS STORED IN VARIABLE
 let cardValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q'];
-let cardSuit = ['H', 'S', 'D', 'C'];
+let cardSuit = ['♠', '♥', '♣', '♦'];
 let cardDeck = [];
 
 // SCORE COUNTER BETWEEN DEALER AND PLAYER 
 let dealerTotal = [];
 let playerTotal = [];
+let playerHand = document.querySelector('#playerText')
 
 // BUTTONS STORED IN A VARIABLE 
 let startButton = document.querySelector('#startButton')
@@ -20,7 +21,8 @@ function newDeck(){
         }
     }
 }
-newDeck(); 
+// newDeck();
+
 
 // A FUNCTION THAT SHUFFLES MY DECK 
 function shuffleDeck (){
@@ -31,13 +33,58 @@ function shuffleDeck (){
         [cardDeck[currentIndex], cardDeck[randomIndex]] = [cardDeck[randomIndex], cardDeck[currentIndex]];
     }
     console.log(cardDeck)
-   return cardDeck
+    return cardDeck
 }
-shuffleDeck();
+// shuffleDeck();
 
-function startGame(){
 
+
+
+
+window.onload = function startGame(){
+    newDeck(); 
+    shuffleDeck();
 }
+
+
+
+
+
+
+
+// startButton.addEventListener('click', startGame)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -50,20 +97,22 @@ function startGame(){
 // hitButton.addEventListener('click', hitButtonFunction);
 
 // function hitButtonFunction (){
-//     shuffleDeck();
-//     // console.log(cardDeck)
-//     // logging first card 
-//     console.log(cardDeck[0])
-// }
-
-hitButton.addEventListener('click', () => {
-    shuffleDeck();
-    // console.log(cardDeck)
-    // logging first card 
-    console.log(cardDeck[0])
-})
-   
-//STAY BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
+    //     shuffleDeck();
+    //     // console.log(cardDeck)
+    //     // logging first card 
+    //     console.log(cardDeck[0])
+    // }
+    
+    hitButton.addEventListener('click', () => {
+        // shuffleDeck();
+        // console.log(cardDeck)
+        // logging first card
+        // console.log(cardDeck[0])
+        // playerHand.innerHTML = `Player ${firstTwo}`
+        
+    })
+    
+    //STAY BUTTON AND FUNCTION FOR WHAT HAPPENS NEXT 
 stayButton.addEventListener('click', () => {
 
 })
