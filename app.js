0281cbafc9a142ed0f4b66e94b8dbf71166e4720
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2022-11-19 12:13:59 -0800

    update hit function

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let cardDeck = [];
 // HANDS THAT DEALER AND PLAYER HAVE
 let dealerHand = [];
 let playerHand = [];
-let hidden = 0;
+// let hidden = 0; // hidden is second dealer card should be added to dealer hand 
 
 //HTML IN A VARIABLE 
 let playerTextOnScreen = document.querySelector('#playerText')
@@ -25,7 +25,6 @@ function newDeck(){
         }
     }
 }
-
 // SHUFFLES MY DECK 
 function shuffleDeck (){
     let currentIndex = cardDeck.length //randomIndex;
@@ -38,26 +37,34 @@ function shuffleDeck (){
     return cardDeck
 }
 
+
 //START BUTTON FUNCTION
 startButton.addEventListener('click', startGame)
-
 
 function startGame(){
     newDeck();
     shuffleDeck();
-    hidden = (cardDeck[Math.floor(Math.random() * cardDeck.length)])
-    console.log(hidden + " hidden number in the hidden array")
-
-    dealerHand =  cardDeck[Math.floor(Math.random() * cardDeck.length)]
-
+    hit(playerHand, cardDeck)
+    hit(dealerHand, cardDeck)
+    hit(playerHand, cardDeck)
+    hit(dealerHand, cardDeck)
+    console.log(playerHand)
+    console.log(cardDeck)
+    
+    console.log(dealerHand + " dealers first card given dealers array")
+    console.log(dealerTotalNumber + " this is hidden number and the dealer total added")
+    
+    // hidden = (cardDeck[Math.floor(Math.random() * cardDeck.length)])
+    // dealerHand =  
+    
+    
     // dealerTotalNumber = hidden.value + dealerHand.value
-    console.log(dealerTotalNumber + " the hidden number and the dealer total added")
-    // console.log(dealerSum)
-    console.log(dealerHand + " dealers first card given")
-    for (let i = 0; i < 2; i++){
-        randomCard = cardDeck[Math.floor(Math.random() * cardDeck.length)] 
-        playerHand.push(randomCard)
-        }
+    // console.log(hidden + " hidden number")
+    // for (let i = 0; i < 2; i++){
+    //     randomCard = cardDeck[Math.floor(Math.random() * cardDeck.length)] 
+    //     playerHand.push(randomCard)
+    //     }
+
     playerTextOnScreen.innerHTML = `Player ${playerHand}`
     dealerTextOnScreen.innerHTML = `Dealer ${dealerHand}`
     console.log(playerHand + " players hand")
@@ -65,14 +72,27 @@ function startGame(){
     calculatePlayerHand()
     console.log(playerTotalNumber + " this is the sum of the player hand") // we do not have to have this number appear on the dom, only have to  have it calculated and ran in a conditional.        
     console.log(cardDeck)
+    // cardDeck[0].charAt(0)
+    console.log(cardDeck[0].charAt(0))
+}
+
+
+function hit (hand, deck){
+    let card = deck.pop()
+    hand.push(card)
 }
 
 
 
-let playerScore = 0;
-//CALCULATE PLAYER HAND
 
+// hit(playerHand, cardDeck);
+// console.log(playerHand)
+
+
+
+//CALCULATE PLAYER HAND
 let playerTotalNumber = 0;
+
 function calculatePlayerHand(){
 for(let i = 0; i < playerHand.length ; i++){
 if ( playerHand[i][0] === "J" || playerHand[i][0] ===     "K" || playerHand[i][0] === "Q" ) {
@@ -93,6 +113,7 @@ playerTotalNumber+=10
 
 //CALCULATE DEALER HAND
 let dealerTotalNumber = 0;
+
 function calculateDealerHand(){
     for(let i = 0; i < dealerHand.length ; i++){
         if ( dealerHand[i][0] === "J" || dealerHand[i][0] === "K" || dealerHand[i][0] === "Q" || dealerHand === 10) {
@@ -120,24 +141,21 @@ hitButton.addEventListener('click', hitButtonFunction);
 
 function hitButtonFunction (){
     calculatePlayerHand()
-    // calculateDealerHand()
+    calculateDealerHand()
     // console.log(dealerHand)
     playerHand += cardDeck[Math.floor(Math.random() * cardDeck.length)];
     playerTextOnScreen.innerHTML = `Player ${playerHand}`
     console.log(playerHand)
-    // console.log(cardDeck + " card Deck ")
-    // // playerTextOnScreen.innerHTML = `Player ${firstTwo}`
     }
 
     
 // //STAY BUTTON 
 stayButton.addEventListener('click', stayButtonFunction )
+
 function stayButtonFunction(){
     winningGame()
     
 }
-
-
 
 
 // LOSE CONDITION 
@@ -149,13 +167,13 @@ function winningGame(){
     } else console.log("BUST")
 }
 
-// function losingGame(){
-//     if(dealerTotalNumber === 21){
-//         return console.log("BlackJack")
-//     } else if (dealerTotalNumber < 21) {
-//         console.log("Keep Going")
-//     } else console.log("BUST")
-// }
+function losingGame(){
+    if(dealerTotalNumber === 21){
+        return console.log("BlackJack")
+    } else if (dealerTotalNumber < 21) {
+        console.log("Keep Going")
+    } else console.log("BUST")
+}
 
 
 
